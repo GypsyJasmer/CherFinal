f7dddb7ee26abcb99794470868679b0221751edd
Sandi <59848573+GypsyJasmer@users.noreply.github.com>
2021-03-12 20:23:24 -0800

    Added Timeline Domain Model, issues with Kendo extension. Fashion page links are not opening the view.

---
+++ CherFanPage/CherFanPage/Models/DM/TimelineEventAction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CherFanPage.Models.DM
+{
+    public class TimelineEventAction
+    {
+        public string text { get; set; } // this field name must be "text"
+        public string url { get; set; } // this field name must be "url"
+    }
+}
---
+++ CherFanPage/CherFanPage/Models/DM/TimelineModel.cs
@@ -0,0 +1,25 @@
+﻿using CherFanPage.Models.DM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CherFanPage.Models
+{
+    public class TimelineModel
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Description { get; set; }
+
+
+        public DateTime EventDate { get; set; }
+
+        public List<TimelineEventAction> Actions { get; set; }
+
+
+
+    }
+
+
+}
