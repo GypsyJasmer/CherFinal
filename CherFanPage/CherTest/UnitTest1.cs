27144de573fee4fcccb62e431cf7e389d981a63b
Sandi <59848573+GypsyJasmer@users.noreply.github.com>
2021-03-16 20:53:19 -0700

    Added test for Quiz, had quiz from an older cher project, and everything the same and works.

---
+++ CherFanPage/CherTest/UnitTest1.cs
@@ -0,0 +1,49 @@
+using System;
+using Xunit;
+using CherFanPage.Models;
+
+namespace CherTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        //Test when user gives correct answers
+        public void RightAnswerTest()
+        {
+            //Arrange
+            var quiz = new QuizVM()
+            {
+                UserAnswer1 = "26",
+                UserAnswer2 = "Mask",
+                UserAnswer3 = "1 year",
+                UserAnswer4 = "Moonstruck"
+            };
+
+            //Act
+            quiz.CheckAnswers();
+
+            //Assert
+            Assert.True("Right" == quiz.RightOrWrong1 && "Right" == quiz.RightOrWrong2 && "Right" == quiz.RightOrWrong3 && "Right" == quiz.RightOrWrong4);
+        }
+
+        [Fact]
+        //Test when user gives wrong answers 
+        public void WrongAnswerTest()
+        {
+            //Arrange
+            var quiz = new QuizVM()
+            {
+                UserAnswer1 = "53",
+                UserAnswer2 = "Mermaids",
+                UserAnswer3 = "2 years",
+                UserAnswer4 = "Mask"
+            };
+
+            //Act
+            quiz.CheckAnswers();
+
+            //Assert
+            Assert.True("Wrong" == quiz.RightOrWrong1 && "Wrong" == quiz.RightOrWrong2 && "Wrong" == quiz.RightOrWrong3 && "Wrong" == quiz.RightOrWrong4);
+        }
+    }
+}
