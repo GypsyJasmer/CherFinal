f11d4d3c961351df8d1cf553d35911382319eb83
Sandi <59848573+GypsyJasmer@users.noreply.github.com>
2021-03-16 22:22:44 -0700

    Last push for DM

--- CherFanPage/CherFanPage/Models/SeedData.cs
+++ CherFanPage/CherFanPage/Models/SeedData.cs
@@ -109,14 +109,14 @@ namespace CherFanPage.Models
                 Outfit JC=new Outfit{ OutfitID = "JC", Title = "Just Cher",  LogoImage = "1960JustCher.png" };
                 context.Outfits.Add(JC);
                 context.SaveChanges();
-                //new { OutfitID = "SC", Title = "Sonny & Cher", Decade = "1960's", LogoImage = "~/FavOutfits/1960SonnynCher.png" };
-                //new { OutfitID = "HB", Title = "Half Breed", Decade = "1970's", LogoImage = "~/FavOutfits/1970HalfBreed.png" };
-                //new { OutfitID = "D1", Title = "Disco 1", Decade = "1970's", LogoImage = "~/FavOutfits/1970Part2.png" };
-                //new { OutfitID = "D2", Title = "Egypt Cher", Decade = "1970's", LogoImage = "~/FavOutfits/1970Random.png" };
-                //new { OutfitID = "D3", Title = "Take Me Home", Decade = "1970's", LogoImage = "~/FavOutfits/1970TakeMeHome.png" };
-                // new { OutfitID = "A1", Title = "Award #1", Decade = "1980's", LogoImage = "~/FavOutfits/1980Award1.png" };
-                //new { OutfitID = "A2", Title = "Bob Mackey", Decade = "1980's", LogoImage = "~/FavOutfits/1980BobM.png" };
-                // new { OutfitID = "2000", Title = "Current Tour Cher", Decade = "2000's", LogoImage = "~/FavOutfits/2000FB.png" };
+                //new { OutfitID = "SC", Title = "Sonny & Cher", Decade = "1960's", LogoImage = "1960SonnynCher.png" };
+                //new { OutfitID = "HB", Title = "Half Breed", Decade = "1970's", LogoImage = "1970HalfBreed.png" };
+                //new { OutfitID = "D1", Title = "Disco 1", Decade = "1970's", LogoImage = "1970Part2.png" };
+                //new { OutfitID = "D2", Title = "Egypt Cher", Decade = "1970's", LogoImage = "1970Random.png" };
+                //new { OutfitID = "D3", Title = "Take Me Home", Decade = "1970's", LogoImage = "1970TakeMeHome.png" };
+                // new { OutfitID = "A1", Title = "Award #1", Decade = "1980's", LogoImage = "1980Award1.png" };
+                //new { OutfitID = "A2", Title = "Bob Mackey", Decade = "1980's", LogoImage = "1980BobM.png" };
+                // new { OutfitID = "2000", Title = "Current Tour Cher", Decade = "2000's", LogoImage = "2000FB.png" };
 
 
 
